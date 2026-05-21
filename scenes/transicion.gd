c60e541acc86f7762b1bfc3fed0eecaf45796b83
Claudia <cbarahona@incomaz.local>
2026-05-21 11:47:06 +0200

    corregido error en transicion

--- scenes/transicion.gd
+++ scenes/transicion.gd
@@ -1,18 +1,18 @@
 extends CanvasLayer
 
-@onready var color = $ColorRect
+@onready var fondo = $TextureRect
 
 func _ready():
-	color.modulate.a = 0.0
+	fondo.modulate.a = 0.0
 
 func fade_in(duracion: float = 1.5) -> void:
-	color.modulate.a = 0.0
+	fondo.modulate.a = 0.0
 	var tween = create_tween()
-	tween.tween_property(color, "modulate:a", 1.0, duracion)
+	tween.tween_property(fondo, "modulate:a", 1.0, duracion)
 	await tween.finished
 
 func fade_out(duracion: float = 0.5) -> void:
-	color.modulate.a = 1.0
+	fondo.modulate.a = 1.0
 	var tween = create_tween()
-	tween.tween_property(color, "modulate:a", 0.0, duracion)
+	tween.tween_property(fondo, "modulate:a", 0.0, duracion)
 	await tween.finished
